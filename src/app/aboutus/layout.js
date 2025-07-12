64afde0a93f9006f4840b1a4b7308df6a090d6cf
Bigbilly-github <abiolatwitter97@gmail.com>
2025-07-12 08:37:30 +0100

    i created the about us route folder, inside of which i created the page.js, layout.js. i also added font "oxygen" to the layout file.and i created the components folder

---
+++ src/app/aboutus/layout.js
@@ -0,0 +1,30 @@
+import { Oxygen } from "next/font/google";
+
+
+const oxygen = Oxygen({
+  subsets: ["latin"],
+   weight: ["300", "400", "700"],
+  variable: "--font-oxygen", 
+});
+
+export const metadata = {
+  title: "Cyncra internship AboutUs Page",
+  description: "General information about Cyncra Internship program",
+};
+
+const AboutUsLayout = () => {
+  return (
+    <>
+        <html lang="en"  >
+            <body
+                className={` ${oxygen.className} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    </>
+   
+  )
+}
+
+export default AboutUsLayout
