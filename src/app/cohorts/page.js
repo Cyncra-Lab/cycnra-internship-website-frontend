d57ff937bbe0dccbdc4fb9fa5fe8469dbbfb4e41
Victor Iberi <iberivictor7@gmail.com>
2025-07-24 08:38:43 +0100

    made some adjustments

--- src/app/cohorts/page.js
+++ src/app/cohorts/page.js
@@ -5,24 +5,13 @@ import React from "react";
 import ProjectsSection from "@/components/cohort/ProjectsSection";
 import Footer from "@/components/aboutus/footer";
 import NavBar from "@/components/NavBar";
-import Image from "next/image";
 
 const Cohort = () => {
   return (
     <div>
-      <div className="fixed top-0 left-0 w-full h-[170px] z-20">
-        <div className="relative w-full h-[100%]">
-          <Image
-            src="/images/navBG.png"
-            alt="Navbar Background"
-            fill
-            className="object-cover"
-            priority
-          />
-          <NavBar/>
-        </div>
+      <div className="bg-[url('/images/navBG.png')] bg-cover bg-center fixed top-0 left-0 w-full h-[168px] z-20">
+        <NavBar />
       </div>
-
       <Hero />
       {/* <CohortInfo /> */}
       <ProjectsSection />
