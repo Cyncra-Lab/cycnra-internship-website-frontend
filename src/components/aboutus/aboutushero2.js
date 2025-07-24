361b9f425f946a09d23edbc638f59810290992e0
Bigbilly-github <abiolatwitter97@gmail.com>
2025-07-24 10:00:00 +0100

    correctional changes and added animations

--- src/components/aboutus/aboutushero2.js
+++ src/components/aboutus/aboutushero2.js
@@ -30,17 +30,17 @@ const Aboutushero2 = () => {
   ];
   return (
     <>
-      <section className="w-full flex justify-center mt-[40px] ">
+      <section className="w-[100%]  flex justify-center mt-[40px] ">
         <div className="w-[90%] h-[572px]  flex flex-col ">
           <div className="flex justify-between items-center ">
-            <p className="font-bold text-[40px] w-[574px] text-[#02353C] leading-[120%] ">
+            <p className="font-bold xl:text-[40px] lg:text-[32px] lg:w-[474px] xl:w-[574px] text-[#02353C] leading-[120%] ">
               Cyncra{" "}
               <span className="font-normal">
                 knows that work shapes our future - so we make sure
               </span>{" "}
               <span className="text-[#FFC857] ">yours counts.</span>
             </p>
-            <p className="w-[400px] h-[99px] text-[20px] leading-[120%] text-[#02353C] tracking-[5%] ">
+            <p className="max-w-[400px] text-[20px] leading-[120%] text-[#02353C] tracking-[5%] ">
               Our internship helps you build skills, confidence and connections
               for a career that matters.
             </p>
@@ -49,23 +49,23 @@ const Aboutushero2 = () => {
             {display.map((items, index) => (
               <div
                 key={index}
-                className={`w-[327px] ${
+                className={` xl:w-[337px] lg:w-[257px] ${
                   index % 2 !== 0
                     ? "bg-[#02353C] self-end"
                     : "bg-[#FFC857] self-start"
-                }  h-[326px]  flex items-center justify-center rounded-[20px]`}
+                }  h-[326px] flex items-center pl-[20px]   rounded-[20px]`}
               >
-                <div className="w-[216px] h-[142px] ">
+                <div className=" h-[142px] flex  flex-col  justify-center  ">
                   <Image src={items.imageicon} alt="card icon" />
                   <p
-                    className={`text-[64px] ${
+                    className={`xl:text-[64px] lg:text-[50px] ${
                       index % 2 !== 0 ? "text-[#E6FAFC]" : "text-[#02353C] "
-                    }   font-bold  leading-[140%] `}
+                    }   font-bold   leading-[140%] `}
                   >
                     {items.number}+
                   </p>
                   <p
-                    className={`text-[16px]  ${
+                    className={`xl:text-[16px] lg:text-[14px]  ${
                       index % 2 !== 0 ? "text-[#E6FAFC]" : "text-[#02353C] "
                     }    `}
                   >
--- src/components/aboutus/design.js
+++ src/components/aboutus/design.js
@@ -5,21 +5,23 @@ const Design = ({ frame1, maintext, subtext }) => {
   return (
     <>
       <div className="flex justify-between items-center">
-        <div className="flex  gap-[30px] items-center">
-           <Image src={frame1} alt="image" className="w-[432px] h-[266px]" />
-            <div className="flex flex-col w-[631px] ">
-              <p className="text-[36px]  leading-[120%]  text-[#E6FAFC]">
+        <div className="flex  gap-[30px] items-center ">
+          <Image
+            src={frame1}
+            alt="image"
+            className="xl:w-[432px] lg:w-[300px] h-[266px]"
+          />
+          <div className="flex flex-col w-[631px] ">
+            <p className="xl:text-[36px] lg:text-[28px]  leading-[120%]  text-[#E6FAFC]">
               {maintext}
-              </p>
-              <p className="h-[136px] text-[20px] leading-[120%] mt-[21px] text-[#E6FAFC] ">
-                {subtext}
-              </p>
-            </div>
-
+            </p>
+            <p className="h-[136px] xl:text-[20px] lg:text-[18px] lg:w-[520px] leading-[120%] mt-[21px] text-[#E6FAFC] ">
+              {subtext}
+            </p>
+          </div>
         </div>
-       
 
-        <button className="h-[232px] focus:outline-2 focus:outline-white border-white  hover:bg-opacity-65 hover:border-[#FFC857] w-[78px] flex items-center justify-center border-[2px] rounded-[50px] ">
+        <button className="xl:h-[232px] lg:h-[192px] focus:outline-2 focus:outline-white border-white  hover:bg-opacity-65 hover:border-[#FFC857] w-[78px] flex items-center justify-center border-[2px] rounded-[50px] ">
           <Image src={caret} alt="caret right icon" />
         </button>
       </div>
