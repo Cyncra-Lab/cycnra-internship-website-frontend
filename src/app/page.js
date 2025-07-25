e7a10a4572f532fafd80b0d2bf45a24c7c9d8480
PrincessCS <princeschidera@gmail.com>
2025-07-25 18:12:01 +0100

    Fix: Uncomment footer in landing page

--- src/app/page.js
+++ src/app/page.js
@@ -31,9 +31,9 @@ export default function Home() {
       </section>
 
       {/* Footer Section */}
-      {/*<section>
+      <section>
         <Footer />
-      </section> */}
+      </section>
     </>
   );
 }
